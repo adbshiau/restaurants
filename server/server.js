b88e95309758ccedbac67d441f6e5c06f27b41d9
AyaShiau <63468278+adbshiau@users.noreply.github.com>
2022-11-08 10:12:38 -0800

    Set up test route to get all restaurants

--- server/server.js
+++ server/server.js
@@ -4,6 +4,12 @@ const app = express();
 
 const port = process.env.PORT || 3001;
 
+// ROUTES
+//? http://localhost:3001/getRestaurants
+app.get("/getRestaurants", (req, res) => {
+  console.log("Get all restaurants!");
+});
+
 app.listen(port, () => {
   console.log(`Server is up and listening on port ${port}`);
 });
