8e6ef2c1f3cf831aa62b54a69010368cc7ada688
AyaShiau <63468278+adbshiau@users.noreply.github.com>
2022-11-08 13:28:26 -0800

    Define skeleton of routes

--- server/server.js
+++ server/server.js
@@ -1,13 +1,64 @@
 require("dotenv").config();
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 const port = process.env.PORT || 3001;
 
-// ROUTES
-//? http://localhost:3001/getRestaurants
-app.get("/getRestaurants", (req, res) => {
-  console.log("Get all restaurants!");
+// MIDDLEWARES
+app.use(morgan("dev"));
+app.use(express.json());
+
+// ROUTE HANDLER
+//? Get all restaurants
+app.get("/api/v1/restaurants", (req, res) => {
+  res.status(200).json({
+    status: "Success",
+    data: {
+      restaurant: ["McDonalds", "In n Out"],
+    },
+  });
+});
+
+//? Get a restaurant
+app.get("/api/v1/restaurants/:id", (req, res) => {
+  console.log(req.params);
+  res.status(200).json({
+    status: "Success",
+    data: {
+      restaurant: ["Sugarfish"],
+    },
+  });
+});
+
+//? Create a restaurant
+app.post("/api/v1/restaurants", (req, res) => {
+  console.log(req.body);
+  res.status(201).json({
+    status: "Success",
+    data: {
+      restaurant: ["Sugarfish"],
+    },
+  });
+});
+
+//? Update a restaurant
+app.put("/api/v1/restaurants/:id", (req, res) => {
+  console.log(req.params.id);
+  console.log(req.body);
+  res.status(200).json({
+    status: "Success",
+    data: {
+      restaurant: ["Sugarfish"],
+    },
+  });
+});
+
+//? Delete a restaurant
+app.delete("/api/v1/restaurants/:id", (req, res) => {
+  res.status(204).json({
+    status: "Success",
+  });
 });
 
 app.listen(port, () => {
