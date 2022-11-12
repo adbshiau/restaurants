b456e5e23f88fa5df91e6e16e2ccf33f9a75eb9a
AyaShiau <63468278+adbshiau@users.noreply.github.com>
2022-11-11 16:16:01 -0800

    Set up react routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { RestaurantDetailPage } from "./routes/RestaurantDetailPage";
+import { Home } from "./routes/Home";
+import { UpdatePage } from "./routes/UpdatePage";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/restaurants/:id/update" element={<UpdatePage />} />
+        <Route
+          exact
+          path="/restaurants/:id"
+          element={<RestaurantDetailPage />}
+        />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
---
+++ client/src/routes/RestaurantDetailPage.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const RestaurantDetailPage = () => {
+  return <div>RestaurantDetailPage</div>;
+};
