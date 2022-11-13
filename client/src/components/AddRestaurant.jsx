dc69d45381d8ae0a468a5ae7b6c7b4bdffb4a882
AyaShiau <63468278+adbshiau@users.noreply.github.com>
2022-11-13 11:35:09 -0800

    Add state variables to AddRestaurant component

--- client/src/components/AddRestaurant.jsx
+++ client/src/components/AddRestaurant.jsx
@@ -1,22 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const AddRestaurant = () => {
+  const [name, setName] = useState();
+  const [location, setLocation] = useState();
+  const [priceRange, setPriceRange] = useState("Price Range");
   return (
     <div className="mb-4">
       <form action="">
         <div className="row">
           <div className="col-4">
-            <input type="text" className="form-control" placeholder="Name" />
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
           </div>
           <div className="col-4">
             <input
               type="text"
               className="form-control"
               placeholder="Location"
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
             />
           </div>
           <div className="col-3">
-            <select className="form-select">
+            <select
+              className="form-select"
+              value={priceRange}
+              onChange={(e) => setPriceRange(e.target.value)}
+            >
               <option disabled>Price Range</option>
               <option value="1">$</option>
               <option value="2">$$</option>
